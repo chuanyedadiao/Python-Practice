ad355540e9fc50b2c3d452ba505b5c13c6375901
chuanyedadiao <51955993+chuanyedadiao@users.noreply.github.com>
2020-02-18 20:30:15 +0800

    Create 1-2.py

---
+++ 1/1-2.py
@@ -0,0 +1,16 @@
+str = 'Runoob'
+
+input("\n\n按下 enter 键后退出。")
+
+print(str)# 输出字符串
+print(str[0:-1])# 输出第一个到倒数第二个的所有字符
+print(str[0])# 输出字符串第一个字符
+print(str[2:5])# 输出从第三个开始到第五个的字符
+print(str[2:])# 输出从第三个开始后的所有字符
+print(str * 2)# 输出字符串两次
+print(str + '你好')# 连接字符串
+
+print('-------------------------')
+
+print('hello\nrunoob')
+print(r'hello\nrunoob')
