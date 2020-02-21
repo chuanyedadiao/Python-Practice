6d703fb42824cae6f20c6656a43245d413f006e6
chuanyedadiao <51955993+chuanyedadiao@users.noreply.github.com>
2020-02-21 19:31:22 +0800

    Create base-30.py

---
+++ base/base-30.py
@@ -0,0 +1,12 @@
+# 引入 datetime 模块
+import datetime
+def getYesterday(): 
+    today=datetime.date.today() 
+    oneday=datetime.timedelta(days=1) 
+    yesterday=today-oneday  
+    return yesterday
+ 
+# 输出
+print(getYesterday())
+
+###得到昨天的日期
