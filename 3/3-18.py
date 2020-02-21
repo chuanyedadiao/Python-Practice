8fae06586af08f278e6846279e78da883bd36208
chuanyedadiao <51955993+chuanyedadiao@users.noreply.github.com>
2020-02-21 14:49:20 +0800

    Create 3-18.py

---
+++ 3/3-18.py
@@ -0,0 +1,10 @@
+##############python3标准库##############
+from datetime import date
+now = date.today()
+print(now)
+print(now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B."))
+
+
+birthday = date(1964, 7, 31)
+age = now - birthday
+print(age.days)
