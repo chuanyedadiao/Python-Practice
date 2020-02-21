b3c70079372f73845425ad08bdb3249bf704e97e
chuanyedadiao <51955993+chuanyedadiao@users.noreply.github.com>
2020-02-21 13:19:28 +0800

    Create 3-11.py

---
+++ 3/3-11.py
@@ -0,0 +1,20 @@
+#生成器
+
+import sys
+ 
+def fibonacci(n): # 生成器函数 - 斐波那契
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n): 
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+f = fibonacci(10) # f 是一个迭代器，由生成器返回生成
+ 
+while True:
+    try:
+        print (next(f), end=" ")
+    except StopIteration:
+        sys.exit()
+##俺现在不是很懂啥意思（迷惑！）
