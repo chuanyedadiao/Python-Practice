96b2cca9253392f7e5d288405edbe59f45b2efa0
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:32:03 +0800

    Create 3-5.py

---
+++ course/3/3-5.py
@@ -0,0 +1,7 @@
+guests = ['zhengqing','wuxing','laowang']
+print(guests[1]+' can\'t come to my dinner')
+
+guests[1] = 'chuanye'
+
+for guest in guests:
+    print('Dear '+ guest +' would you come to enjoy the dinner with me?')
