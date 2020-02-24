a7de509a0473620e2af82867776716122176255f
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:32:59 +0800

    Create 5-2.py

---
+++ course/5/5-2.py
@@ -0,0 +1,45 @@
+print('chuanye' == 'xiaochuan')
+
+print("\n\n")
+
+print('chuanye' != 'xiaochuan')
+
+print("\n\n")
+
+print(1 == 1)
+
+print("\n\n")
+
+print(1 >= 1)
+
+print("\n\n")
+
+print(1 != 1)
+
+print("\n\n")
+
+print(1 <= 1)
+
+print("\n\n")
+
+print(1 > 1)
+
+print("\n\n")
+
+print(1 < 1)
+
+print("\n\n")
+
+print(1 == 1 and 'chuanye' != 'xiaochuan')
+
+print("\n\n")
+
+print(1 != 1 or 'chuanye' != 'xiaochuan')
+
+list_test = list(range(1,11,2))
+
+print("\n\n")
+print(3 in list_test)
+
+print("\n\n")
+print(2 in list_test)
