43189a7058ada37885d7bf528a92d4106afad271
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:32:57 +0800

    Create 5-11.py

---
+++ course/5/5-11.py
@@ -0,0 +1,12 @@
+num = list(range(1,10))
+
+for i in num:
+    if i == 1:
+        print(str(i)+'st')
+    elif i == 2:
+        print(str(i)+'nd')
+    elif i == 3:
+        print(str(i)+'rd')
+    else:
+        print(str(i)+'th')
+
