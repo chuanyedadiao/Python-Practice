7a87740e0943383a1e0978b2e99b59ecb7b8986b
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:33:12 +0800

    Create 5-8.py

---
+++ course/5/5-8.py
@@ -0,0 +1,7 @@
+username = ['chuanye','xiaozhong','zhouxiang','xiaocui','admin']
+
+for user in username:
+    if user == 'admin':
+        print('Hello admin,would you like to see a status report?')
+    else :
+        print('Hello '+user+' ,thank you for logging in again')
