c102337e3660323c73bea7353af43ddc465255ed
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:32:30 +0800

    Create 4-13.py

---
+++ course/4/4-13.py
@@ -0,0 +1,11 @@
+foods = ('french fries','cola','chips','pizza','milk tea')
+
+for food in foods:
+    print(food)
+
+#######food[0] = 'instaqnt noodle'
+
+foods = ('french fries','cola','chips','humberger','latiao')
+
+for food in foods:
+    print(food)
