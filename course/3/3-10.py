e0b676acb3fc69bcf017f22f59f748d8cdf22752
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:31:52 +0800

    Create 3-10.py

---
+++ course/3/3-10.py
@@ -0,0 +1,9 @@
+fancies = ['SPYAIR','japan','animation','Adele\'s life','Gintama','Fiona&Steve','Shamless']
+
+print(sorted(fancies))
+
+fancies.reverse()
+print(fancies)
+
+fancies.sort()
+print(fancies)
