13fc38ef526320a4a7cc7638f44bbd230b5a3a4f
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:31:41 +0800

    Create 2-6.py

---
+++ course/2/2-6.py
@@ -0,0 +1,4 @@
+famous_person1 = 'Fiona'
+famous_person2 = 'Steve'
+message = '\'I trust you. It\'s more important than I love you. It\'s a bigger deal.\''
+print(famous_person1 + ' said' + message +'to ' +famous_person2)
