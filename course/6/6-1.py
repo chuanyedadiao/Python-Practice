d6bd31600bc4045ff16100b0874ebbc986bb6ede
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:33:17 +0800

    Create 6-1.py

---
+++ course/6/6-1.py
@@ -0,0 +1,9 @@
+people = {'first_name':'kawago',
+          'last_name':'xu',
+          'age':21,
+          'city':'ChangSha'}
+
+print(people['first_name'])
+print(people['last_name'])
+print(people['age'])
+print(people['city'])
