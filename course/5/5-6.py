349bcf57a0356be99a58b315ca241ed1a43a0da2
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:33:08 +0800

    Create 5-6.py

---
+++ course/5/5-6.py
@@ -0,0 +1,14 @@
+age = 21
+
+if age<2 :
+    print('He is a baby')
+elif age<4 :
+    print('蹒跚学步')
+elif age<13:
+    print('他是儿童')
+elif age<20:
+    print('他是青少年')
+elif age<65:
+    print('他是成年人')
+elif age>=65:
+    print('他是老年人')
