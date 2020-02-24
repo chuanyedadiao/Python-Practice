dda2ebdfc6189dce616baec3c0fb27204f09f1e2
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:33:20 +0800

    Create 6-2.py

---
+++ course/6/6-2.py
@@ -0,0 +1,10 @@
+favorite_num = {'chuanye':4,
+                'xiaozhong':7,
+                'zhengqing':3,
+                'xiaocui':8,
+                'siwen':0}
+print(favorite_num['chuanye'])
+print(favorite_num['xiaozhong'])
+print(favorite_num['zhengqing'])
+print(favorite_num['xiaocui'])
+print(favorite_num['siwen'])
