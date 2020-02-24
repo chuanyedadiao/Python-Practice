fd51c25dc9c00a1143cbdf99e5ff1e39d4ee4ea6
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-24 22:33:06 +0800

    Create 5-5.py

---
+++ course/5/5-5.py
@@ -0,0 +1,23 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("Player got 5 points")
+elif alien_color == 'yellow':
+    print("Player got 10 points")
+else:
+    print("Player got 15 points")
+    
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("Player got 5 points")
+elif alien_color == 'yellow':
+    print("Player got 10 points")
+else:
+    print("Player got 15 points")
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("Player got 5 points")
+elif alien_color == 'yellow':
+    print("Player got 10 points")
+else:
+    print("Player got 15 points")
