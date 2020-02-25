27f780524150c25d3775f8928cf7dc0f344e908b
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 14:53:35 +0800

    Create 8-4.py

---
+++ course/8/8-4.py
@@ -0,0 +1,6 @@
+def make_shirt(size = "L",lable = "I love python"):
+    print("Make the "+size+" shirt with printing "+"\" "+lable+" \"")
+
+make_shirt()
+make_shirt(size = "M")
+make_shirt(size = "S",lable = "I fancy you" )
