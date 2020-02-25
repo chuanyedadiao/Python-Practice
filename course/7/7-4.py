d68c5e33f2333b6891dbceb1a41ed2599d4e9ddf
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 12:25:33 +0800

    Create 7-4.py

---
+++ course/7/7-4.py
@@ -0,0 +1,6 @@
+infor = "init"
+
+while infor!="quit":
+    infor = input("\nPlease enter the thing you want to add to the pizza:(Or you can quit)")
+    if infor != "quit":
+        print("Add "+infor+" to pizza successfully")
