ae3e52de6a408291828d2070d65b637f6245da45
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 12:25:29 +0800

    Create 7-2.py

---
+++ course/7/7-2.py
@@ -0,0 +1,6 @@
+people_num = int(input("How many people are there going to have dinner?"))
+
+if people_num > 8:
+    print("There is no table available")
+else:
+    print("There are some table available")
