0936e5ba9d934500b067c57e3bad3a6b62b9c186
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 14:53:37 +0800

    Create 8-5.py

---
+++ course/8/8-5.py
@@ -0,0 +1,6 @@
+def describe_city(name="Sealto",nation="America"):
+    print(name + " is in "+ nation)
+
+describe_city()
+describe_city(name = "ChangDe",nation = "China")
+describe_city("San Francisco")
