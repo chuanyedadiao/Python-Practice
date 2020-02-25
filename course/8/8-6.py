119a954f32af834623bfdf3eb86e43fbd5231d92
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 14:53:38 +0800

    Create 8-6.py

---
+++ course/8/8-6.py
@@ -0,0 +1,8 @@
+def city_country(name,nation):
+    infor = name+" , "+nation
+    return infor
+
+
+print(city_country("changde","China"))
+print(city_country("San Francisco","America"))
+print(city_country("Xi'An","China"))
