2b9bd119cb2ccdb761f1c50732fa21384dcf90fe
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 11:07:13 +0800

    Create 6-9.py

---
+++ course/6/6-9.py
@@ -0,0 +1,6 @@
+favorite_places = {'chuanye':'Japan',
+                   'xiaozhong':'America',
+                   'zhengqing':'New Zealand'}
+
+for key,value in favorite_places.items():
+    print(key + "'s favorite_place is "+value)
