9562f37e995b64bd0571cafe7f59022b8e24502b
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 11:06:48 +0800

    Create 6-5.py

---
+++ course/6/6-5.py
@@ -0,0 +1,11 @@
+rivers = {'nile':'egypt',
+          'Changjiang':'China',
+          'Mississipi':'America'}
+for key,value in rivers.items():
+    print('The '+key+' run through '+value)
+
+for key in rivers.keys():
+    print(key)
+
+for value in rivers.values():
+    print(value)
