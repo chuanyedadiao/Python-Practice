80beaeeff689be6b07f69e9af4e9bda5a02cd325
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 11:06:46 +0800

    Create 6-3.py

---
+++ course/6/6-3.py
@@ -0,0 +1,7 @@
+dic = {'if else':'selection',
+       'while':'circle',
+       'for':'circle',
+       'str':'string',
+       'input':'shuru'}
+for key,value in dic.items():
+    print(key+': '+value)
