1009c07e14eef7383d1be2789521448922ac51e5
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 14:53:32 +0800

    Create 8-3.py

---
+++ course/8/8-3.py
@@ -0,0 +1,6 @@
+def make_shirt(size,lable):
+    print("Make the "+size+" shirt with printing "+"\" "+lable+" \"")
+
+make_shirt("L","everybody always leaves")
+
+make_shirt(lable="everybody always leaves",size="L")
