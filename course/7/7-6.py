da78e151ca573c142be29bd641b402aeb65fa3c4
chuanyedadiao <chuanyedadiao@gmail.com>
2020-02-25 12:25:37 +0800

    Create 7-6.py

---
+++ course/7/7-6.py
@@ -0,0 +1,17 @@
+active = True
+
+while active:
+    infor = input("\nPlease enter the thing you want to add to the pizza:(Or you can quit)")
+    if infor != "quit":
+        print("Add "+infor+" to pizza successfully")
+    else:
+        active = False
+
+
+
+while True:
+    infor = input("\nPlease enter the thing you want to add to the pizza:(Or you can quit)")
+    if infor != "quit":
+        print("Add "+infor+" to pizza successfully")
+    else:
+        break
