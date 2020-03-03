28468be9933e732a2a2828aa7bb86d5e0f682eb7
chuanyedadiao <chuanyedadiao@gmail.com>
2020-03-03 14:47:11 +0800

    Update game_stats.py

--- course/alien_invasion/game_stats.py
+++ course/alien_invasion/game_stats.py
@@ -2,7 +2,12 @@ class GameStats():
     def __init__(self,ai_settings):
         self.ai_settings=ai_settings
         self.reset_stats()
-        self.game_active = True
+        self.game_active = False
+        self.score = 0
+
+        self.init_score = 0
+        self.high_score = 0
+        self.level = 1
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_limit
