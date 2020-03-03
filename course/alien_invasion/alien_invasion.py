bf2903085cb9651d2937440d2714680e60d8526e
chuanyedadiao <chuanyedadiao@gmail.com>
2020-03-03 14:47:03 +0800

    Update alien_invasion.py

--- course/alien_invasion/alien_invasion.py
+++ course/alien_invasion/alien_invasion.py
@@ -5,6 +5,8 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
     #初始化游戏 并且创建一个屏幕对象
@@ -14,18 +16,20 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen,stats)
     ship = Ship(ai_settings,screen)
     bullets = Group()
     aliens = Group()
     gf.create_fleet(ai_settings,screen,ship,aliens)
+    play_button = Button(ai_settings,screen,"Play")
     #开始游戏的主循环
     while True:
         
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+            gf.update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,screen,stats,sb,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
 
 run_game()
